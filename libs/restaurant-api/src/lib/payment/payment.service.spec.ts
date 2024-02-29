1824b7badc3980baf8c2b45f2f2e1b2dae93afb0
Vitaliy Karetin <vitaliykaretin@Vitaliis-MacBook-Pro.local>
2024-02-29 18:40:24 +0200

    UPD: test providers

--- libs/restaurant-api/src/lib/payment/payment.service.spec.ts
+++ libs/restaurant-api/src/lib/payment/payment.service.spec.ts
@@ -1,12 +1,30 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { PaymentService } from "./payment.service";
+import { PAYMENTS_REPOSITORY, PAYMENT_METHODS_REPOSITORY } from "./constants";
+import { StripeService } from "../stripe/stripe.service";
+import { ConfigService } from "@nestjs/config";
+
+const paymentsRepositoryMock = {};
+const paymentMethodsRepositoryMock = {};
 
 describe("PaymentService", () => {
 	let service: PaymentService;
 
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
-			providers: [PaymentService],
+			providers: [
+				PaymentService,
+				StripeService,
+				ConfigService,
+				{
+					provide: PAYMENTS_REPOSITORY,
+					useValue: paymentsRepositoryMock
+				},
+				{
+					provide: PAYMENT_METHODS_REPOSITORY,
+					useValue: paymentMethodsRepositoryMock
+				}
+			],
 		}).compile();
 
 		service = module.get<PaymentService>(PaymentService);
